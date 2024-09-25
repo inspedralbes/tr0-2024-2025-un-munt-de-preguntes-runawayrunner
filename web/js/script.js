b5d0b09f19193405198eafde994156b9a264dd92
Aivan Antonio <a21aivantjeh@inspedralbes.cat>
2024-09-26 01:37:57 +0200

    he afegit quan es mostra si la resposta es correcte o fals

--- web/js/script.js
+++ web/js/script.js
@@ -1,16 +1,18 @@
-function començarPartit() {
-    fetch('http://localhost/TR0_UMDP/back/getPreguntes.php')
-        .then(response => response.json())
-        .then(info => {     
-            console.log(info);
-            mostrarPregunta(info);
-        })
-        .catch(error => {
-            console.error('Error al cargar las preguntas:', error);
-        });
-    
-    function mostrarPregunta(data) {    
-        let preguntaActual = 0;
+let preguntaActual = 0;
+let dadesPreguntes = [];
+
+fetch('http://localhost/TR0_UMDP/back/getPreguntes.php')
+    .then(response => response.json())
+    .then(info => {    
+        console.log(info);
+        dadesPreguntes = info;
+        mostrarPregunta(dadesPreguntes, preguntaActual);
+    })
+    .catch(error => {
+        console.error('Error al cargar las preguntas:', error);
+    });
+
+    function mostrarPregunta(data, preguntaActual) {
         let htmlString = '';
     
         if (preguntaActual < data.length) {
@@ -22,17 +24,34 @@ function començarPartit() {
             htmlString += `<table><tr>`;
             for (let indexResposta = 0; indexResposta < opcions.length; indexResposta++) {
                 resposta = pregunta.respostes[indexResposta];
-                htmlString += `<td><button onclick="processarResposta()">${opcions[indexResposta]}</button></td><td>${resposta.resposta}</td></tr>`;
+                htmlString += `<td><button onclick="processarResposta(${preguntaActual}, ${indexResposta})">${opcions[indexResposta]}</button></td><td>${resposta.resposta}</td></tr>`;
             }
         }
         document.getElementById("partida").innerHTML = htmlString;
     }
+    function processarResposta(preguntaActual, indexResposta) {
+        console.log(`Pregunta actual: ${preguntaActual}, Respuesta seleccionada: ${indexResposta}`);
+        preguntaActual++;
+        let htmlStringResposta = '';
 
-    function processarResposta() {
-        console.log(`HOLA`);
-        //console.log(`Pregunta: ${indexResposta + 1}, Opcion seleccionada: ${pregunta.respostes[indexResposta].resposta}`);
-        
+        if (pregunta.respostes[indexResposta].correcta) {
+            htmlStringResposta += `<h4>Resposta correcte</h4>`;
+        } else {
+            htmlStringResposta += `<h4>Resposta incorrecte</h4>`;
+        }
+
+        const resultatRespostaElement = document.getElementById("resposta");
+        if (resultatRespostaElement) {
+            resultatRespostaElement.innerHTML = htmlStringResposta;
+        } else {
+            console.error("El elemento 'resposta' no se encontró en el DOM.");
+        }
+
+        document.getElementById("resultatResposta").innerHTML = htmlStringResposta;
+
+        setTimeout(() => {
+            mostrarPregunta(dadesPreguntes, preguntaActual);
+        }, 3000); 
     }
-}
 
     
